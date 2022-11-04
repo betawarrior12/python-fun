a24e1dd002d0dc30140a87dac4282644ab4d144c
Sourcery AI <>
2022-11-04 23:29:19 +0000

    'Refactored by Sourcery'

--- webapps/flask/app.py
+++ webapps/flask/app.py
@@ -23,7 +23,6 @@ def home():
     # todo_list = Todo.query.all()
     todo_list = db.session.query(Todo).all()
     return "Hello, World!"
-    return render_template("base.html", todo_list=todo_list)
 
 
 # @app.route("/add", methods=["POST"])
